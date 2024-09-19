a6fe762c98727e05f2f7e216afaa0f3e1b6c4ec6
Sara <saravanessaguedesof@gmail.com>
2024-09-19 09:42:12 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem(resposta) {
+    const mensagem = resposta === 'Sim' ? 'Que bom que você está/dormiu bem!' : 'Vamos encontrar uma maneira de te animar, gatinha!';
+    alert(mensagem);
+  }
